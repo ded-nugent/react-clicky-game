f92e09c111323c98621574777688d5e23b8f10fd
ded-nugent <maxwellwesterberg1995@gmail.com>
2020-01-11 10:39:41 -0600

    Updates

---
+++ precache-manifest.8b1ac4239af8f258ed129ae819af4c28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b775197b69d748c069bc18b25644dd05",
+    "url": "/react-clicky-game/index.html"
+  },
+  {
+    "revision": "ef8418dc596ea6ba6027",
+    "url": "/react-clicky-game/static/css/main.9e585ded.chunk.css"
+  },
+  {
+    "revision": "93ae42b0e0bd6d7dd7ad",
+    "url": "/react-clicky-game/static/js/2.1c1c9579.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-clicky-game/static/js/2.1c1c9579.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ef8418dc596ea6ba6027",
+    "url": "/react-clicky-game/static/js/main.e8a6431d.chunk.js"
+  },
+  {
+    "revision": "4f203b3164f809861cc0",
+    "url": "/react-clicky-game/static/js/runtime-main.93c69817.js"
+  }
+]);
